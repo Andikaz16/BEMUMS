4d290e608ec4d59ea6073af200f023bacab6d042
Andikaz16 <yogaa3486@gmail.com>
2026-08-14 16:06:21 +0700

    fix buug

--- src/components/AdminCMS.jsx
+++ src/components/AdminCMS.jsx
@@ -152,7 +152,7 @@ export default function AdminCMS({ db, onUpdateDB }) {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        const img = new Image();
+        const img = new window.Image();
         img.onload = () => {
           const canvas = document.createElement('canvas');
           let width = img.width;
